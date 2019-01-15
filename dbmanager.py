af5df9c62c55dc1fb43edcf3b18e0b8a8bf3ecea
QiusenHuangGT <qiusenhuang17@hotmail.com>
2019-01-15 14:23:19 -0500

    create dbmanager, create population script, create db that has data from 2018 fall

--- dbmanager.py
+++ dbmanager.py
@@ -1,1 +1,48 @@
 ##database manager
+
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+#Prevent posible package not found error
+import sqlite3
+
+sqlite_file = 'my_db.sqlite'                #filename for database
+
+class DatabaseManager:
+    def __init__(self):
+        self.__conn = sqlite3.connect(sqlite_file)
+        self.__cur = self.__conn.cursor()
+        self.create()
+
+    def create(self):
+        command = '''CREATE TABLE IF NOT EXISTS JOKES (
+            Comedian text,
+            Joke text,
+            Charlen integer,
+            Wordlen integer
+        )
+        '''
+        self.__cur.execute(command)
+        self.__conn.commit()
+
+    def execute(self, command):
+        self.__cur.execute(command)
+
+    def fetch(self):
+        return self.__cur.fetchone()
+
+    def fetch_all(self):
+        return self.__cur.fetchall()
+
+    def commit(self):
+        return self.__conn.commit()
+
+    def close(self):
+        return self.__conn.close()
+
+
+
+
+
---
+++ population.py
@@ -0,0 +1,33 @@
+#this can fill db with data from local file 
+#or restore the data in the database in case someone messes up the db
+import csv
+from dbmanager import DatabaseManager
+
+name = 'Final CSV  - Sheet1.csv'
+
+#parse data from csv
+def populate(filename):
+    data = []
+    with open(filename) as file:
+        reader = csv.reader(file, delimiter=',')
+        line = 0
+        for row in reader:
+            if line != 0:                           #excluding column title
+                data.append(row)
+            line += 1
+    return data
+
+if __name__ == '__main__':
+    temp_data = populate(name)
+    db_manager = DatabaseManager()
+    for i in temp_data:
+        command = f'''INSERT INTO JOKES VALUES (
+            "{i[0]}",
+            "{i[3].replace('"', "'")}",
+            {i[1]},
+            {i[2]}
+        )
+        '''
+        db_manager.execute(command)
+        db_manager.commit()
+    db_manager.close()
